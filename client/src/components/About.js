fcb5248f63f88ed31f87ad84cf7192a52d3d4a06
FuChai Mead <jessie.mf.wolff@gmail.com>
2018-01-06 13:27:31 -0700

    about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Header, Segment, Container, Image, Grid } from 'semantic-ui-react';
+import slc from '../images/salt-lake-city.jpg';
 
 class About extends Component {
   render() {
@@ -8,24 +9,34 @@ class About extends Component {
       <Segment basic> 
       <Header as='h1' textAlign='center'> Welcome to Red House</Header>
       <center> <i> authentic Sichuan and Chongqing style food in Salt Lake City </i></center>
-      <Image src='/assets/images/wireframe/image.png' size='small' alt='salt lake city' />
       </Segment> 
         <Grid divided='vertically'>
           <Grid.Row columns={2}>
             <Grid.Column>
-              <Segment> 
-              <strong> Location </strong> 
-              <p> Red House </p> 
-              <p> 1465 S State St East Side </p>
-              <p> Salt Lake City </p>  
-              </Segment> 
+      <Image src={slc} alt='salt lake city' style={styles.image} />
             </Grid.Column>
             <Grid.Column>
+              <Segment > 
+              <strong> Location </strong> 
+              <p style={styles.p}> Red House </p> 
+              <p style={styles.p}> 1465 S State St East Side </p>
+              <p style={styles.p}> Salt Lake City </p>  
+              </Segment> 
+              <Segment > 
+              <strong> About Us </strong> 
+              <p> 在这里吃中国菜</p> 
+              <p style={styles.p}> 1465 S State St East Side </p>
+              <p style={styles.p}> Salt Lake City </p>  
+              </Segment> 
             <Segment> 
               <strong> Hours </strong> 
-              <p> Red House </p> 
-              <p> 1465 S State St East Side </p>
-              <p> Salt Lake City </p>  
+              <p style={styles.p}> Monday: 11:30am - 9:30pm </p> 
+              <p style={styles.p}> Tuesday: 5:00pm - 9:30pm </p> 
+              <p style={styles.p}> Wednesday: 11:30am - 9:30pm </p> 
+              <p style={styles.p}> Thursday: 11:30am - 9:30pm </p> 
+              <p style={styles.p}> Friday: 11:30am - 9:30pm </p> 
+              <p style={styles.p}> Saturday: 11:30am - 9:30pm </p> 
+              <p style={styles.p}> Sunday: 11:30am - 9:30pm </p> 
               </Segment> 
             </Grid.Column>
           </Grid.Row>
@@ -34,6 +45,16 @@ class About extends Component {
 
     );
   }
+}
+
+const styles = {
+  image: {
+    width: '100%'
+  },
+  p: {
+    margin: '0',
+    padding:'0'
+}
 }
 
 export default About;
