473a851ad50aa1bdaaa8513a661a5eef85823e1d
Christopher Lazard <christopher.l.lazard@gmail.com>
2018-01-06 13:26:42 -0700

    sync

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import { Menu, Segment } from 'semantic-ui-react';
+import { Segment, Container, Grid, List, Header } from 'semantic-ui-react'
 
-class About extends Component {
-  render() {
-    return(
-      <div> Footer </div> 
+       
+       class Footer extends Component {
+         render() {
+          return (
+
+         
+       <div>
+       <Segment inverted vertical style={{ padding: '5em 0em' }}>
+          <Container>
+            <Grid divided inverted stackable>
+              <Grid.Row>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='About' />
+                  <List link inverted>
+                    <List.Item as='a'>Sitemap</List.Item>
+                    <List.Item as='a'>Contact Us</List.Item>
+                    <List.Item as='a'>BitCoin</List.Item>
+                    <List.Item as='a'>LockNess</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='Services' />
+                  <List link inverted>
+                    <List.Item as='a'>The Earth is Flat</List.Item>
+                    <List.Item as='a'>2+2=5</List.Item>
+                    <List.Item as='a'>How To Access</List.Item>
+                    <List.Item as='a'>Why is the sky blue?</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={7}>
+                  <Header as='h4' inverted>We Hope...</Header>
+                  <p>That you enjoy our humble attempt of a bird based social media website. </p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
+      </div>
     )
   }
 }
 
-export default About;
+
+export default Footer;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Segment } from 'semantic-ui-react';
+import Wallpaper from '../images/Wallpaper.jpg';
 
 class Home extends Component {
   render() {
     return (
+      <div style={styles.image}>
+      <Segment style={styles.opacity}>
       <Header as='h1' textAlign='center'>Home Component</Header>
+      </Segment>
+      </div>
     );
+  }
+}
+
+const styles = {
+  image: {
+    backgroundImage: "url("+ Wallpaper + ")",
+    backgroundSize: "cover"
+  },
+  opacity: {
+    backgroundColor: "rgba(200, 200, 200, 0)",
+    height: "100vh"
   }
 }
 
