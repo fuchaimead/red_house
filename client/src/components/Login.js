a9c02d0a1402ddde92cbc7a61533282a2d3f7986
Amy Stevens <amystevens62288@gmail.com>
2018-01-06 14:46:23 -0700

    login page styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Header, Segment, Form, Button } from 'semantic-ui-react';
+import { Header, Segment, Form, Button, Container } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { handleLogin } from '../actions/auth';
+import Wallpaper from '../images/Wallpaper.jpg';
 
 class Login extends Component {
   state = { email: '', password: '' };
@@ -21,10 +22,12 @@ class Login extends Component {
   render() {
     const { email, password } = this.state;
     return (
-      <Segment basic>
+      <div style={styles.image}>
+      <Segment style={styles.opacity}>
         <Header as='h1' textAlign='center'>Login</Header>
         <Form onSubmit={this.handleSubmit}>
           <Form.Field>
+          <Container>
             <label htmlFor='email'>Email</label>
             <input
               required
@@ -32,9 +35,10 @@ class Login extends Component {
               value={email}
               placeholder='Email'
               onChange={this.handleChange}
-            />
+            /> </Container>
           </Form.Field>
           <Form.Field>
+          <Container>
             <label htmlFor='password'>Password</label>
             <input
               required
@@ -43,14 +47,27 @@ class Login extends Component {
               placeholder='Password'
               type='password'
               onChange={this.handleChange}
-            />
+            /></Container>
           </Form.Field>
           <Segment textAlign='center' basic>
             <Button primary type='submit'>Submit</Button>
           </Segment>
         </Form>
+
       </Segment>
+      </div>
     );
+  }
+}
+
+const styles = {
+  image: {
+    backgroundImage: "url("+ Wallpaper + ")",
+    backgroundSize: "cover"
+  },
+  opacity: {
+    backgroundColor: "rgba(200, 200, 200, 0)",
+    height: "100vh"
   }
 }
 
