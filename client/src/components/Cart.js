a4a37407d68aeb4b7f728fb48ae005b364c94759
Max Winslow <maxtwinslow1@gmail.com>
2018-01-06 13:25:29 -0700

    Wallpaper plus Cart Modal

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { setHeaders } from '../actions/headers'
 import { connect } from 'react-redux'
+import { Button, Modal, Header, Icon } from 'semantic-ui-react'
 
 class Cart extends Component {
 
-  state = { cartItems: [] }
+  state = { cartItems: [], modalOpen: false }
 
   componentWillMount(){
     axios.get('/api/index_cart')
@@ -16,13 +17,46 @@ class Cart extends Component {
   }
 
   displayCartItem = () => {
-
+    return this.state.cartItems.map( each => {
+      return (
+        <li>
+          {each.name} => {each.price}
+        </li>
+      )
+    })
   }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+  handleClose = () => this.setState({ modalOpen: false})
+
+
 
   render(){
     return(
       <div>
-        Cart Goes Here
+
+
+
+      <ul>
+        {this.displayCartItem()}
+      </ul>
+        <Modal
+          trigger={<Button onClick={this.handleOpen}>Basic Modal</Button>}
+          basic size=
+          'small'
+          open={this.state.modalOpen}
+          onClose={this.handleClose}
+          >
+          <Header icon='archive' content='Confirm Your Order' />
+            <Modal.Content>
+              {this.displayCartItem()}
+            </Modal.Content>
+            <Modal.Actions>
+            <Button color='green' onClick={this.handleClose} inverted>
+              <Icon name='checkmark' /> Yes
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </div>
   )
   }
